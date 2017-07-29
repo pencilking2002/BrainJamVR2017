126ce95f03bf59a140cc9bc4ec9285f620352545
SCOTT TONGUE <STONGUE87@GMAIL.COM>
2017-07-29 19:32:46 -0400

    video changes

---
+++ Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Neuromancers
+{
+    [RequireComponent(typeof(UnityEngine.Video.VideoPlayer))]
+    public class VideoPlayer : MonoBehaviour
+    {
+
+        UnityEngine.Video.VideoPlayer vrPlayer;
+
+        public string youtube360;
+        public bool testMode = false;
+       // Use this for initialization
+       protected void Awake()
+       {
+        }
+
+        protected void Start()
+        {
+            vrPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+            if(!testMode)
+            vrPlayer.url = youtube360;
+        }
+
+
+        public void PlayVideoVR()
+        {
+            vrPlayer.Play();
+        }
+   
+    }
+}
