a0c997fa543f0d9b888216f3773bb1e00b660a86
Andrew Garrahan <andrew@computerlunch.com>
2017-07-29 11:48:21 -0400

    random scene with 200 nodes

--- Assets/Scripts/BrainController.cs
+++ Assets/Scripts/BrainController.cs
@@ -8,25 +8,50 @@ public class BrainController : MonoBehaviour {
 
 	public List<Neuron> nodes = new List<Neuron>();
 
+	private GameObject nodeGo;
+
 	void SetUp(){
+
+			nodeGo = new GameObject();
+			nodeGo.name = "| Neurons |";
 
 			GameObject go;
 		
-			float offSetX = 10;
-			float offSetY = 10;
+			//float offSetX = 3;
+			//float offSetY = 3;
 		
 			// Place nodes
-			for( int i = 0; i < 10 ; i++){
+			for( int i = 0; i < 200 ; i++){
 
 				go = Instantiate(Resources.Load("Neuron")) as GameObject;
 
-				Neuron node = go.AddComponent<Neuron>();
+				Neuron node = go.GetComponent<Neuron>();
 				nodes.Add(node);
 
-				go.transform.position = new Vector3(i * offSetX, 0, 0 );
+				go.transform.position = nodeRandomPlacement();//new Vector3(i * offSetX, 0, 0 );
+
+				go.transform.parent = nodeGo.transform;
 			}
 
 
+	}
+
+	Vector3 nodeRandomPlacement()
+	{
+		Vector3 centerVec = new Vector3( 0,0,0);
+		
+		float rangeX = 10;
+		float rangeY = 10;
+		float rangeZ = 10;
+
+		Vector3 randomPos = new Vector3(
+				Random.Range(rangeX * -1, rangeX),
+				Random.Range(rangeY * -1, rangeY), 
+				Random.Range(rangeZ * -1, rangeZ));
+
+		randomPos += centerVec; // add center point offset
+
+		return randomPos;
 	}
 
 	/*
