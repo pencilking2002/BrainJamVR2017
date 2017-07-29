5ba35d6c338749bb0cb34126218410a4fee872a2
SCOTT TONGUE <STONGUE87@GMAIL.COM>
2017-07-29 12:51:43 -0400

    working

---
+++ Assets/Scripts/NeruonTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Neuromancers
+{
+    [RequireComponent(typeof(SphereCollider))]
+    public class NeruonTrigger : MonoBehaviour
+    {
+        //readonly
+        //Serialized
+        
+
+        /////Protected/////
+        //References
+        //Primitives
+
+        ///////////////////////////////////////////////////////////////////////////
+        //
+        // Inherited from MonoBehaviour
+        //
+
+        protected void Awake()
+        {
+        }
+
+        protected void Start()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        //
+        // #SCRIPTNAME# Functions
+        //
+
+        ////////////////////////////////////////
+        //
+        // Function Functions
+    }
+}
--- Assets/Scripts/Neuron.cs
+++ Assets/Scripts/Neuron.cs
@@ -1,16 +1,112 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Neuron : MonoBehaviour {
+namespace Neuromancers
+{
+    public class Neuron : MonoBehaviour
+    {
+       
+        [Tooltip("Put child trigger object here")]
+        public GameObject neuronTigger;
+        public List<Neuron> neighbors = new List<Neuron>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        /////Protected/////
+        //References
+        //Primitives
+        private bool neighborNode = false;
+
+        public bool NeighborNode
+        {
+            get { return neighborNode; }
+            set
+            {
+                neighborNode = value;
+                NeigborNodesSelectable(neighborNode);
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////////
+        //
+        // Inherited from MonoBehaviour
+        //
+
+    protected void Awake()
+    {
+    }
+
+    protected void Start()
+    {
+            if (!neuronTigger)
+                Debug.LogError("Missing Trigger object");
+    }
+
+    protected void Update()
+    {
+
+    }
+
+   /// <summary>
+   /// TURN on/off Highligher for neighbor nodes
+   /// </summary>
+   /// <param name="turnOn"></param>
+    private void NeigborNodesSelectable(bool turnOn)
+    {
+
+            if (turnOn)
+            {
+                //TODO: TURN ON HIGHTLIGHTER
+            }
+            else
+            {
+                //TODO: TURN OFF HightLIGHTER
+            }
+           
+    }
+    
+
+        /// <summary>
+        /// Selecting a node for input
+        /// </summary>
+    public void SelectNode()
+    {
+            //Check to see if we are in neighbor selection
+            if (neighborNode)
+            {
+
+            }
+           else //normal selection
+            {
+                if (!neuronTigger)
+                {
+                    neuronTigger.SetActive(true);
+                    Destroy(neuronTigger, 1f);
+                }
+
+            }
+
+    }
+
+   public void MakeNeighborsList(Neuron )
+   {
+
+   }
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // #SCRIPTNAME# Functions
+    //
+
+        ////////////////////////////////////////
+        //
+        // Function Functions
+    }
 }
